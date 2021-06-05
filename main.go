5ab61ca9bde6d030dc1d9a64a04cb94119e61239
Vinny Sabatini <vincent.sabatini@gmail.com>
2021-06-05 18:06:24 -0500

    separate home and contact pages

--- main.go
+++ main.go
@@ -7,7 +7,11 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Hello world!</h1>")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Hello world!</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:vincent.sabatini@gmail.com\">vincent.sabatini@gmail.com</a>")
+	}
 }
 
 func main() {
