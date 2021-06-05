e4f43a086af1b467f3cef5cff0427b1eec241ff4
Vinny Sabatini <vincent.sabatini@gmail.com>
2021-06-05 18:21:31 -0500

    add 404 page

--- main.go
+++ main.go
@@ -11,6 +11,9 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "<h1>Hello world!</h1>")
 	} else if r.URL.Path == "/contact" {
 		fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:vincent.sabatini@gmail.com\">vincent.sabatini@gmail.com</a>")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<h1>This page was not found</h1><p>Please let us know if you keep being sent to an invalid page</p>")
 	}
 }
 
