8ad0d5350a546db5006c262af7d10e47373f4a12
Vinny Sabatini <vincent.sabatini@gmail.com>
2021-06-26 14:17:10 -0500

    create simple faq page
    This is for chapter 3 exercise 1

--- main.go
+++ main.go
@@ -12,6 +12,12 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:vincent.sabatini@gmail.com\">vincent.sabatini@gmail.com</a>")
 }
 
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>FAQ</h1>")
+	fmt.Fprint(w, "<p>This page is still in development</p>")
+}
+
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>Hello world!</h1>")
@@ -21,5 +27,6 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
 	http.ListenAndServe(":3000", r)
 }
