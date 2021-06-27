1935f33448b7e604e25066251e0685617b4e72ae
Vinny Sabatini <vincent.sabatini@gmail.com>
2021-06-26 21:05:44 -0500

    create first instance with gotemplate

--- main.go
+++ main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
+
+type User struct {
+	Name string
+}
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
@@ -30,11 +35,26 @@ func notfound(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<p>Eh you lost there bud?</p>")
 }
 
+func templatePage(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	t, err := template.ParseFiles("templates/hello.gohtml")
+	if err != nil {
+		panic(err)
+	}
+
+	data := User{
+		Name: "Howie",
+	}
+
+	fmt.Fprint(w, t.Execute(w, data))
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.NotFoundHandler = http.HandlerFunc(notfound)
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
 	r.HandleFunc("/faq", faq)
+	r.HandleFunc("/templatePage", templatePage)
 	http.ListenAndServe(":3000", r)
 }
