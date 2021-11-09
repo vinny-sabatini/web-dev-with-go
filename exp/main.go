a8070e13639eb37a1dd8dc993e6a7b30d1d71315
Vinny Sabatini <vincent.sabatini@kohls.com>
2021-11-09 16:53:49 -0600

    play with sql more
    Do example of joining tables, load some data into the orders table. This
    completes "Handling relational data with Go's sql package" in chapter 8

--- exp/main.go
+++ exp/main.go
@@ -8,11 +8,16 @@ import (
 )
 
 const (
-	host     = "localhost"
-	port     = 5432
-	user     = "vinnysabatini"
-	password = "notrealpassword"
-	dbname   = "vinnysabatini"
+	host = "localhost"
+	port = 5432
+	// Personal Laptop
+	// user     = "vinnysabatini"
+	// password = "notrealpassword"
+	// dbname   = "vinnysabatini"
+	// Work Laptop
+	user     = "postgres"
+	password = ""
+	dbname   = "postgres"
 )
 
 func main() {
@@ -23,26 +28,34 @@ func main() {
 	}
 	defer db.Close()
 
-	type User struct {
-		ID    int
-		Name  string
-		Email string
-	}
-	var users []User
-	rows, err := db.Query(`
-		SELECT *
-		FROM users`)
+	// Create sample order data
+	// for i := 1; i < 6; i++ {
+	// 	userID := 1
+	// 	if i > 3 {
+	// 		userID = 2
+	// 	}
+	// 	amount := i * 100
+	// 	description := fmt.Sprintf("USB-C Adapber x%d", i)
+	// 	_, err = db.Exec(`INSERT INTO orders(user_id, amount, description) VALUES($1, $2, $3)`, userID, amount, description)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// }
+
+	// Joining tables
+	rows, err := db.Query(`SELECT * FROM users INNER JOIN orders on users.id = orders.user_id`)
 	if err != nil {
 		panic(err)
 	}
-	defer rows.Close()
 	for rows.Next() {
-		var user User
-		err = rows.Scan(&user.ID, &user.Name, &user.Email)
-		if err != nil {
+		var userID, orderID, amount int
+		var email, name, desc string
+		if err := rows.Scan(&userID, &name, &email, &orderID, &userID, &amount, &desc); err != nil {
 			panic(err)
 		}
-		users = append(users, user)
+		fmt.Println("userID:", userID, "name:", name, "email:", email, "orderID:", orderID, "amount:", amount, "desc:", desc)
 	}
-	fmt.Println(users)
+	if rows.Err() != nil {
+		panic(rows.Err())
+	}
 }
