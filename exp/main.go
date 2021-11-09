72db69e051d675cd480900f27282fb73eaf0759e
Vinny Sabatini <vincent.sabatini@kohls.com>
2021-11-09 17:40:57 -0600

    first gorm model
    Create users GORM users model. This table will be deleted everytime the
    codebase is run to ensure we clear our db while testing. This completes
    section "Creating our first model with GORM" in chapter 8

--- exp/main.go
+++ exp/main.go
@@ -20,6 +20,12 @@ const (
 	dbname   = "postgres"
 )
 
+type User struct {
+	gorm.Model
+	Name  string
+	Email string `gorm:"not null;unique_index"`
+}
+
 func main() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d password=%s user=%s dbname=%s sslmode=disable", host, port, password, user, dbname)
 	db, err := gorm.Open("postgres", psqlInfo)
@@ -30,4 +36,7 @@ func main() {
 	if err := db.DB().Ping(); err != nil {
 		panic(err)
 	}
+
+	db.DropTableIfExists(&User{}) // For testing, clear database
+	db.AutoMigrate(&User{})
 }
