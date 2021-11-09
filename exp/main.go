b0d1b0fa0047f247daa5ac4d1ba3ffb00c365f53
Vinny Sabatini <vincent.sabatini@kohls.com>
2021-11-09 17:17:51 -0600

    start the transition to using gorm
    Completes "Setting up GORM" in chapter 8

--- exp/main.go
+++ exp/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 
-	_ "github.com/lib/pq"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 const (
@@ -22,40 +22,12 @@ const (
 
 func main() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d password=%s user=%s dbname=%s sslmode=disable", host, port, password, user, dbname)
-	db, err := sql.Open("postgres", psqlInfo)
+	db, err := gorm.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
-
-	// Create sample order data
-	// for i := 1; i < 6; i++ {
-	// 	userID := 1
-	// 	if i > 3 {
-	// 		userID = 2
-	// 	}
-	// 	amount := i * 100
-	// 	description := fmt.Sprintf("USB-C Adapber x%d", i)
-	// 	_, err = db.Exec(`INSERT INTO orders(user_id, amount, description) VALUES($1, $2, $3)`, userID, amount, description)
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
-	// }
-
-	// Joining tables
-	rows, err := db.Query(`SELECT * FROM users INNER JOIN orders on users.id = orders.user_id`)
-	if err != nil {
+	if err := db.DB().Ping(); err != nil {
 		panic(err)
-	}
-	for rows.Next() {
-		var userID, orderID, amount int
-		var email, name, desc string
-		if err := rows.Scan(&userID, &name, &email, &orderID, &userID, &amount, &desc); err != nil {
-			panic(err)
-		}
-		fmt.Println("userID:", userID, "name:", name, "email:", email, "orderID:", orderID, "amount:", amount, "desc:", desc)
-	}
-	if rows.Err() != nil {
-		panic(rows.Err())
 	}
 }
--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.16
 require (
 	github.com/gorilla/mux v1.8.0
 	github.com/gorilla/schema v1.2.0
+	github.com/jinzhu/gorm v1.9.16 // indirect
 	github.com/lib/pq v1.10.3
 )
